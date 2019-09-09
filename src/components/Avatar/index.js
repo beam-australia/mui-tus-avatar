831394545cc5d3b92ca64863da605d53409b502f
Andrew McLagan <andrewmclagan@gmail.com>
2019-09-09 17:06:08 +1000

    Migrating base cmp to PurComponent

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -35,6 +35,7 @@ class Avatar extends React.PureComponent {
       croppable,
       uploadable
     } = this.props;
+    const { image, open } = this.state;
     return (
       <React.Fragment>
         <Image
